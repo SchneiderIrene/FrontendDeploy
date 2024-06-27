8d13e9644c42f3e6f17d39019c4bda20641c3f0e
AIT TR Student <arbikulova@gmail.com>
2024-06-28 00:04:04 +0200

    almost adaptive design

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,15 +1,19 @@
 import { FAQs } from "assets"
 import styled from "@emotion/styled"
 import { Link } from "react-router-dom"
-import Icon6 from 'assets/open_icon.svg'
+import Icon6 from "assets/open_icon.svg"
 
 export const HomePageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 40px;
+  padding: 40px;
   width: 100%;
   gap: 20px;
+  box-sizing: border-box;
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
 `
 
 export const Container1 = styled.div`
@@ -17,18 +21,19 @@ export const Container1 = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  max-width: 100%; 
+  max-width: 1400px;
   margin: 0 auto;
   margin-top: -20px;
   padding-left: 10px;
   padding-bottom: 100px;
   box-sizing: border-box;
-  overflow: auto; 
+  flex-wrap: wrap;
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: center;
+    padding-bottom: 60px;
   }
-`;
+`
 
 export const Container2 = styled.div`
   display: flex;
@@ -37,11 +42,12 @@ export const Container2 = styled.div`
   width: 100%;
   max-width: 1400px;
   margin-top: -50px;
-  padding-bottom: 60px;
+  padding-bottom: 120px;
   position: relative;
   box-sizing: border-box;
   @media (max-width: 768px) {
     flex-direction: column;
+    margin-top: 0;
   }
 `
 
@@ -60,18 +66,27 @@ export const OverlayContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   position: absolute;
-  top: 0px;
-  margin-bottom: 30px;
+  top: 0;
   left: 0;
   width: 100%;
   z-index: 2;
-
   & > *:first-of-type {
     margin-left: 220px;
   }
-
   & > * + * {
     margin-left: -40px;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    position: static;
+    & > *:first-of-type {
+      margin-left: 0;
+    }
+    & > * + * {
+      margin-left: 0;
+      margin-top: 20px;
+    }
   }
 `
 
@@ -81,16 +96,21 @@ export const Square = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 8px;
-  width: 360px;
-  max-width: 548px; 
-  height: 200px;
-  padding: 25px 41px 25px 40px;
+  width: 100%;
+  max-width: 548px;
+  height: auto;
+  padding: 25px 40px;
   margin-top: 212px;
   margin-left: 16px;
   box-sizing: border-box;
-  background: #FAFAFA;
-  box-shadow: 4px 4px 10px 0px rgba(55, 62, 58, 0.20);
+  background: #fafafa;
+  box-shadow: 4px 4px 10px 0px rgba(55, 62, 58, 0.2);
   backdrop-filter: blur(5px);
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    margin-left: 0;
+    padding: 20px;
+  }
 `
 
 export const RectangleContainer = styled.div`
@@ -99,6 +119,18 @@ export const RectangleContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 172px;
+  padding-left: 30px;
+  width: 100%;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    position: static;
+    & > *:first-of-type {
+      margin-top: 0;
+    }
+    & > * + * {
+      margin-top: 20px;
+    }
+  }
 `
 
 export const Rectangle = styled.div`
@@ -106,16 +138,20 @@ export const Rectangle = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
-  width: 368px;
+  width: 100%;
+  max-width: 368px;
   height: 54px;
   margin-top: 19px;
   margin-left: 54px;
   padding: 4px;
   gap: 0px;
   box-sizing: border-box;
-  background: #FAFAFA;
-  box-shadow: 4px 4px 10px 0px rgba(55, 62, 58, 0.20);
+  background: #fafafa;
+  box-shadow: 4px 4px 10px 0px rgba(55, 62, 58, 0.2);
   backdrop-filter: blur(5px);
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
 `
 
 export const TextColumn = styled.div`
@@ -133,7 +169,7 @@ export const TextWrapper = styled.div`
   font-family: "DM Sans";
   font-size: 18px;
   font-style: normal;
-  color: #373E3A;
+  color: #373e3a;
   margin: 10px;
 `
 
@@ -142,9 +178,10 @@ export const Container3 = styled.div`
   justify-content: space-between;
   gap: 12px;
   margin-top: 24px;
-  max-width: 100%;
+  max-width: 1400px;
+  width: 100%;
+  padding: 2px;
   box-sizing: border-box;
-  overflow: hidden;
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: center;
@@ -156,17 +193,24 @@ export const Image1 = styled.img`
   height: auto;
   object-fit: contain;
   margin-top: -30px;
-  margin-left: 80px;
+  position: relative;
+  left: 30px;
   @media (max-width: 768px) {
     width: 100%;
   }
 `
 
 export const Image3 = styled.img`
-  width: 400px;
-  height: 440px;
-  margin: 12px;
-  @media (max-width: 768px) {
+  width: 360px;
+  height: auto;
+  margin: 6px; 
+  @media (max-width: 1200px) {
+    width: 300px;
+  }
+  @media (max-width: 900px) {
+    width: 200px;
+  }
+  @media (max-width: 600px) {
     width: 100%;
   }
 `
@@ -189,7 +233,7 @@ export const StyledH1Container1 = styled.h1`
   font-family: Inter;
   font-size: 36px;
   font-weight: 700;
-  color: #373E3A;
+  color: #373e3a;
   margin-top: -50px;
   max-width: 500px;
   width: 100%;
@@ -200,10 +244,10 @@ export const StyledH1Container1 = styled.h1`
 export const StyledH2Container2 = styled.h2`
   font-size: 24px;
   font-family: Inter;
-  color: #373E3A;
+  color: #373e3a;
   font-weight: 600;
-  margin-top: -272px;
-  margin-right: 32px;
+  margin-top: -220px;
+  margin-right: 80px;
   max-width: 500px;
   width: 100%;
   word-wrap: break-word;
@@ -217,7 +261,7 @@ export const StyledH2Container2 = styled.h2`
 export const StyledPContainer1 = styled.p`
   display: flex;
   justify-content: space-between;
-  color: #373E3A;
+  color: #373e3a;
   font-family: "DM Sans";
   font-size: 18px;
   font-weight: 400;
@@ -233,14 +277,17 @@ export const StyledH3 = styled.h2`
   font-family: Inter;
   font-size: 28px;
   font-weight: 600;
-  color: #373E3A;
-  margin-top: 60px;
+  color: #373e3a;
+  margin-top: 80px;
+  @media (max-width: 768px) {
+    margin-top: 100px;
+  }
 `
 
 export const StyledPContainer4 = styled.div`
   display: flex;
   justify-content: space-between;
-  color: #373E3A;
+  color: #373e3a;
   text-align: left;
   font-family: Inter;
   font-size: 24px;
@@ -254,22 +301,23 @@ export const StyledPContainer4 = styled.div`
 
 export const StyledFAQHeading = styled.h1`
   display: flex;
-  color: #373E3A;
+  color: #373e3a;
   font-family: Inter;
+  margin-top: 50px;
   font-size: 24px;
   font-weight: 600;
   line-height: normal;
-`;
+`
 
 export const StyledFAQSubHeading = styled.h3`
   display: flex;
-  color: #373E3A;
+  color: #373e3a;
+  margin-bottom: -30px;
   font-family: Inter;
   font-size: 18px;
   font-weight: 450;
   line-height: normal;
-`;
-
+`
 
 export const StyledPContainer5 = styled.div`
   display: flex;
@@ -298,7 +346,7 @@ export const BackgroundImage = styled.img`
 `
 
 export const StyledH5 = styled.h2`
-  color: #373E3A;
+  color: #373e3a;
   font-family: Inter;
   font-size: 24px;
   font-weight: 600;
@@ -307,7 +355,7 @@ export const StyledH5 = styled.h2`
 `
 
 export const StyledP5 = styled.h3`
-  color: #373E3A;
+  color: #373e3a;
   font-family: "DM Sans";
   font-size: 18px;
   font-weight: 400;
@@ -318,10 +366,10 @@ export const StyledLink = styled(Link)`
   font-size: 18px;
   color: #ba2d00;
   font-weight: 600;
-  text-decoration: none; 
+  text-decoration: none;
   margin-left: 5px;
   &:hover {
-    text-decoration: underline; 
+    text-decoration: underline;
   }
 `
 
@@ -337,28 +385,27 @@ export const StyledP = styled.p`
   padding: 30px;
   border-bottom: 2px solid green;
   color: #373e3a;
-  z-index: 2;  
+  z-index: 2;
 `
 
 export const Icon6Styled = styled.img`
   width: 32px;
   height: auto;
   position: relative;
-  top: -36px; 
+  top: -36px;
   margin-left: 2px;
 `
 
 export const StyledSpan = styled.span`
-  color: #BA2D00;
+  color: #ba2d00;
   position: absolute;
   left: 0;
-  font-family: 'Merriweather', serif; 
+  font-family: "Merriweather", serif;
   font-size: 16px;
   font-weight: 600;
-  font-feature-settings: 'lnum';
-  border-bottom: 2px solid green; 
-  `
-
+  font-feature-settings: "lnum";
+  border-bottom: 2px solid green;
+`
 
 export const ButtonControl = styled.div`
   display: flex;
@@ -374,8 +421,8 @@ export const Icon = styled.img`
   height: auto;
 `
 export const StyledEmail = styled.p`
-  color: #BA2D00;
+  color: #ba2d00;
   font-family: "DM Sans";
   font-size: 16px;
   font-weight: 700;
-  `
+`
